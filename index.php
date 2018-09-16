b271ddb111c9704d61b067ed9f9195998c7ad409
Nhan Tran-Phan <nhan.t.tranphan@gmail.com>
2018-09-16 16:47:13 -0500

    adding cors to php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header( 'Location: /www/index.html' ) ; ?>
+<?php 
+header( 'Location: /www/index.html' ); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+?>
