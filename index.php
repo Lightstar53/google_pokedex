8b6318432e6bff52b036da89d561bde3bfd5f2e4
Nhan Tran-Phan <nhan.t.tranphan@gmail.com>
2018-09-23 11:50:19 -0500

    Reformat index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-header( 'Location: /www/index.html' ); 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Location: /www/index.html'); 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header('Access-Control-Allow-Headers: X-Requested-With');
 ?>
